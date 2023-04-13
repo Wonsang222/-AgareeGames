bffe10ee4c3a2a8a04f13c62bb24171e70800659
Benny <wonsang222@naver.com>
2023-04-14 01:52:55 +0900

    First

---
+++ RunnersHi/Util/Http/HttpBase.swift
@@ -0,0 +1,49 @@
+//
+//  HttpBase.swift
+//  RunnersHi
+//
+//  Created by 황원상 on 2023/04/14.
+//
+
+import Foundation
+
+// builder로 구현.
+// header에 보낼 자료들
+struct HttpBaseResource {
+    let reqMethod:String
+    let shouldHandleCookie:Bool
+    let isMultiPart:Bool
+    let params:[String:String]
+}
+
+class ResourceBuilder{
+    private var reqMethod:String = ""
+    private var shouldHandleCookie = true
+    private var isMultiPart = true
+    private var params:[String:String] = [String:String]()
+    
+    func setReqMethod(_ method:String) -> Self{
+        self.reqMethod = method
+        return self
+    }
+    
+    func setCookie(_ bool:Bool)->Self{
+        self.shouldHandleCookie = bool
+        return self
+    }
+    
+    func setMultiPart(_ bool:Bool)->Self{
+        self.isMultiPart = bool
+        return self
+    }
+    
+    func setParams(_ param:String...) ->Self{
+        self.params[param[0]] = param[1]
+        return self
+    }
+
+    func build() -> HttpBaseResource{
+        return HttpBaseResource(reqMethod: reqMethod, shouldHandleCookie: shouldHandleCookie, isMultiPart: isMultiPart, params: params)
+    }
+}
+
